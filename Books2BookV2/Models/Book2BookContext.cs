3ddd9d1c4693b0c913cdef7559edf96ac03ac639
Vidurl16 <62283032+Vidurl16@users.noreply.github.com>
2022-10-20 19:35:55 +0200

    WIP: Added a lot of files, we have an error we need to solve that has to do with login issues that came up after rescaffolding once the DB was changed

--- Books2BookV2/Models/Book2BookContext.cs
+++ Books2BookV2/Models/Book2BookContext.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using Books2BookV2.Data;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
