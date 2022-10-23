decf8dce1510bfbbcdfea4accef5f4a6b6105d10
Vidurl16 <62283032+Vidurl16@users.noreply.github.com>
2022-10-23 09:49:27 +0200

    WIP-Catagory

--- Books2BookV2/Pages/Index.cshtml.cs
+++ Books2BookV2/Pages/Index.cshtml.cs
@@ -1,20 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Books2BookV2.Models;
 
 namespace Books2BookV2.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly Book2BookContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(Book2BookContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
+        public  IEnumerable<TblBook> codingBooks { get; set; } 
         public void OnGet()
         {
-
+             codingBooks = from book in _context.TblBooks
+                              where book.Category == "Coding"
+                              select book;
         }
     }
 }
