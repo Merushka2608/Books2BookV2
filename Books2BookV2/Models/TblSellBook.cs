498c0624391cda0e3e969e38dff96e4e3e440c8b
Merushka Munsamy <Merushka.M@catin.co.za>
2022-10-28 09:50:45 +0200

    Vidur - Fixed some database shit, merushka need to send me new database

---
+++ Books2BookV2/Models/TblSellBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Books2BookV2.Models
+{
+    [Keyless]
+    [Table("tblSellBooks")]
+    public partial class TblSellBook
+    {
+        [Column("ID")]
+        public int Id { get; set; }
+        [StringLength(50)]
+        [Unicode(false)]
+        public string Title { get; set; } = null!;
+        public byte[] FrontPicture { get; set; } = null!;
+        public byte[] BackPicture { get; set; } = null!;
+    }
+}
