a2658808864a2e317cdcfe01ebb58dbeca6a9b39
Merushka Munsamy <Merushka.M@catin.co.za>
2022-11-01 16:16:22 +0200

    Sales Frontend and Stats in progress

---
+++ Books2BookV2/Pages/Sales.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Books2BookV2.Models;
+
+namespace Books2BookV2.Pages
+{
+    public class SalesModel : PageModel
+    {
+        private readonly Book2BookContext _context;
+
+        public SalesModel(Book2BookContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
---
+++ Books2BookV2/Pages/Stats.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Books2BookV2.Models;
+
+namespace Books2BookV2.Pages
+{
+    public class StatsModel : PageModel
+    {
+        private readonly Book2BookContext _context;
+
+        public StatsModel(Book2BookContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<TblBook> codingBooks { get; set; }
+        public IEnumerable<TblBook> businessBooks { get; set; }
+        public IEnumerable<TblBook> fictionBooks { get; set; }
+        public void OnGet()
+        {
+            codingBooks = from book in _context.TblBooks
+                          where book.Category == "Coding"
+                          select book;
+
+            businessBooks = from book in _context.TblBooks
+                            where book.Category == "Business"
+                            select book;
+            fictionBooks = from book in _context.TblBooks
+                           where book.Category == "Fiction"
+                           select book;
+        }
+
+    }
+}
