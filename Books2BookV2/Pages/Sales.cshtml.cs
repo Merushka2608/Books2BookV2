f9b81d807f1028ba06636d8c598742c1e952d5e3
Merushka Munsamy <Merushka.M@catin.co.za>
2022-11-03 13:54:50 +0200

    Sales page and rating in progress

--- Books2BookV2/Pages/Sales.cshtml.cs
+++ Books2BookV2/Pages/Sales.cshtml.cs
@@ -2,17 +2,64 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Books2BookV2.Models;
+using Microsoft.AspNetCore.Http;
+using System.Diagnostics;
+using System.IO;
 
 namespace Books2BookV2.Pages
 {
-    public class SalesModel : PageModel
-    {
-        private readonly Book2BookContext _context;
+	public class SalesModel : PageModel
+	{
+		private readonly Book2BookContext _context;
 
-        public SalesModel(Book2BookContext context)
+		public SalesModel(Book2BookContext context)
+		{
+			_context = context;
+		}
+
+      /*  [HttpGet]
+
+        public IActionResult Index()
         {
-            _context = context;
+            return View();
         }
 
+        [HttpPost]
+
+        public IActionResult Index(IFormFile formFile)
+
+        {
+
+            try
+
+            {
+
+                string fileName = formFile.FileName;
+
+                fileName = Path.GetFileName(fileName);
+
+                string uploadpath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\salesImages", fileName);
+
+                var stream = new FileStream(uploadpath, FileMode.Create);
+
+                formFile.CopyToAsync(stream);
+
+                ViewBag.FrontPicture = "File uploaded successfully.";
+
+            }
+
+            catch
+
+            {
+
+                ViewBag.FrontPicture = "Error while uploading the files.";
+
+            }
+
+            return View();
+
+        }*/
+
     }
+
 }
