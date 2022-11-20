140e6d205346796b08de860797102a469541120d
Merushka Munsamy <Merushka.M@catin.co.za>
2022-11-20 13:35:48 +0200

    Minor edits and scaffolded

--- Books2BookV2/Controllers/TblBooksController.cs
+++ Books2BookV2/Controllers/TblBooksController.cs
@@ -17,203 +17,203 @@ namespace Books2BookV2.Controllers
 {
 
 
-    public class TblBooksController : Controller
-    {
-        private readonly UserManager<IdentityUser> _userManager;
+	public class TblBooksController : Controller
+	{
+		private readonly UserManager<IdentityUser> _userManager;
 
-        private readonly Book2BookContext _context;
+		private readonly Book2BookContext _context;
 
-        public TblBooksController(Book2BookContext context, UserManager<IdentityUser> userManager)
-        {
-            _context = context;
-            _userManager = userManager;
-        }
-
-       
-        public async Task<IActionResult> Index(string searchString, string sortBy, string filterBy)
-        {
-            //  ViewBag.SortCategoryParameter = string.IsNullOrEmpty(sortBy) ? "Title desc":"";
-
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier); // will give the user's userId
-
-            var books = from m in _context.TblBooks
-                         select m;
-
-            if (!String.IsNullOrEmpty(searchString))
-            {
-                books = books.Where(s => s.Title!.Contains(searchString) || s.Isbn!.Contains(searchString));
-            }
-
-            if (!String.IsNullOrEmpty(filterBy))
-            {
-                if(filterBy != "All")
-                {
-                    books = books.Where(s => s.Category!.Contains(filterBy));
-                }
-
-               
-            }
-
-            return View(await books.ToListAsync());
-        }
+		public TblBooksController(Book2BookContext context, UserManager<IdentityUser> userManager)
+		{
+			_context = context;
+			_userManager = userManager;
+		}
 
 
-        // GET: TblBooks
-        /*      public async Task<IActionResult> Index()
+		public async Task<IActionResult> Index(string searchString, string sortBy, string filterBy)
+		{
+			//  ViewBag.SortCategoryParameter = string.IsNullOrEmpty(sortBy) ? "Title desc":"";
+
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier); // will give the user's userId
+
+			var books = from m in _context.TblBooks
+						select m;
+
+			if (!String.IsNullOrEmpty(searchString))
+			{
+				books = books.Where(s => s.Title!.Contains(searchString) || s.Isbn!.Contains(searchString));
+			}
+
+			if (!String.IsNullOrEmpty(filterBy))
+			{
+				if (filterBy != "All")
+				{
+					books = books.Where(s => s.Category!.Contains(filterBy));
+				}
+
+
+			}
+
+			return View(await books.ToListAsync());
+		}
+
+
+		// GET: TblBooks
+		/*      public async Task<IActionResult> Index()
               {
                     return View(await _context.TblBooks.ToListAsync());
               }*/
 
 
 
-        public TblBook GetBookById(int id)
-        {
-            return _context.TblBooks.FirstOrDefault(s => s.BookId == id);   
-        }
-
-
-        // GET: TblBooks/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.TblBooks == null)
-            {
-                return NotFound();
-            }
-
-            var tblBook = await _context.TblBooks
-                .FirstOrDefaultAsync(m => m.BookId == id);
-            if (tblBook == null)
-            {
-                return NotFound();
-            }
-
-            return View(tblBook);
-        }
-
-        // GET: TblBooks/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: TblBooks/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("BookId,Isbn,Title,Category,InStock,Edition,Pages,Condition,NumberOfTimesBorrowed,Description,AverageRating,DatePublished,AuthorId")] TblBook tblBook)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(tblBook);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(tblBook);
-        }
-        //
-		[HttpPost]
-        public ActionResult AddReview(String rating)
+		public TblBook GetBookById(int id)
 		{
-            string rate = rating.ToString();
-            Response.WriteAsync(rate);
-            return View();
+			return _context.TblBooks.FirstOrDefault(s => s.BookId == id);
 		}
-        //
 
 
-        // GET: TblBooks/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.TblBooks == null)
-            {
-                return NotFound();
-            }
+		// GET: TblBooks/Details/5
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null || _context.TblBooks == null)
+			{
+				return NotFound();
+			}
 
-            var tblBook = await _context.TblBooks.FindAsync(id);
-            if (tblBook == null)
-            {
-                return NotFound();
-            }
-            return View(tblBook);
-        }
+			var tblBook = await _context.TblBooks
+				.FirstOrDefaultAsync(m => m.BookId == id);
+			if (tblBook == null)
+			{
+				return NotFound();
+			}
 
-        // POST: TblBooks/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("BookId,Isbn,Title,Category,InStock,Edition,Pages,Condition,NumberOfTimesBorrowed,Description,AverageRating,DatePublished,AuthorId")] TblBook tblBook)
-        {
-            if (id != tblBook.BookId)
-            {
-                return NotFound();
-            }
+			return View(tblBook);
+		}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(tblBook);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TblBookExists(tblBook.BookId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(tblBook);
-        }
+		// GET: TblBooks/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        // GET: TblBooks/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.TblBooks == null)
-            {
-                return NotFound();
-            }
+		// POST: TblBooks/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("BookId,Isbn,Title,Category,InStock,Edition,Pages,Condition,NumberOfTimesBorrowed,Description,AverageRating,DatePublished,AuthorId")] TblBook tblBook)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(tblBook);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(tblBook);
+		}
+		//
+		[HttpPost]
+		public ActionResult AddReview(String rating)
+		{
+			string rate = rating.ToString();
+			Response.WriteAsync(rate);
+			return View();
+		}
+		//
 
-            var tblBook = await _context.TblBooks
-                .FirstOrDefaultAsync(m => m.BookId == id);
-            if (tblBook == null)
-            {
-                return NotFound();
-            }
 
-            return View(tblBook);
-        }
+		// GET: TblBooks/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null || _context.TblBooks == null)
+			{
+				return NotFound();
+			}
 
-        // POST: TblBooks/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.TblBooks == null)
-            {
-                return Problem("Entity set 'Book2BookContext.TblBooks'  is null.");
-            }
-            var tblBook = await _context.TblBooks.FindAsync(id);
-            if (tblBook != null)
-            {
-                _context.TblBooks.Remove(tblBook);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+			var tblBook = await _context.TblBooks.FindAsync(id);
+			if (tblBook == null)
+			{
+				return NotFound();
+			}
+			return View(tblBook);
+		}
 
-        private bool TblBookExists(int id)
-        {
-          return _context.TblBooks.Any(e => e.BookId == id);
-        }
+		// POST: TblBooks/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("BookId,Isbn,Title,Category,InStock,Edition,Pages,Condition,NumberOfTimesBorrowed,Description,AverageRating,DatePublished,AuthorId")] TblBook tblBook)
+		{
+			if (id != tblBook.BookId)
+			{
+				return NotFound();
+			}
+
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(tblBook);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!TblBookExists(tblBook.BookId))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(tblBook);
+		}
+
+		// GET: TblBooks/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null || _context.TblBooks == null)
+			{
+				return NotFound();
+			}
+
+			var tblBook = await _context.TblBooks
+				.FirstOrDefaultAsync(m => m.BookId == id);
+			if (tblBook == null)
+			{
+				return NotFound();
+			}
+
+			return View(tblBook);
+		}
+
+		// POST: TblBooks/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			if (_context.TblBooks == null)
+			{
+				return Problem("Entity set 'Book2BookContext.TblBooks'  is null.");
+			}
+			var tblBook = await _context.TblBooks.FindAsync(id);
+			if (tblBook != null)
+			{
+				_context.TblBooks.Remove(tblBook);
+			}
+
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+		private bool TblBookExists(int id)
+		{
+			return _context.TblBooks.Any(e => e.BookId == id);
+		}
 
 
 
@@ -253,51 +253,60 @@ namespace Books2BookV2.Controllers
         public ActionResult BorrowBook(int BookId)
         {
 
-            DateTime DateBorrow = DateTime.Now;
+			DateTime DateBorrow = DateTime.Now;
 
-            string userId = User.Identity.Name;
-            var user =  _userManager.FindByNameAsync(userId);
-            bool isPaid = false;
-            TblBorrow borrow = new TblBorrow(BookId, userId, DateBorrow, isPaid );
+			string userId = User.Identity.Name;
+			var user = _userManager.FindByNameAsync(userId);
+			bool isPaid = false;
+			TblBorrow borrow = new TblBorrow(BookId, userId, DateBorrow, isPaid);
 
-            //gets all the books where the user has not paid for as yet
-            var booksToPayFor = from b in _context.TblBorrows
-                                where (b.BookId == BookId && b.UserName == userId)
-                                select b;
-            List<TblBorrow> itemsToPayFor = booksToPayFor.ToList<TblBorrow>();
-
-
-            _context.TblBorrows.AddAsync(borrow);
-            _context.SaveChangesAsync();
-
-            return RedirectToAction("Index", "TblBooks");
-
-        }
+			//gets all the books where the user has not paid for as yet
+			var booksToPayFor = from b in _context.TblBorrows
+								where (b.BookId == BookId && b.UserName == userId)
+								select b;
+			List<TblBorrow> itemsToPayFor = booksToPayFor.ToList<TblBorrow>();
 
 
-        
-        public ActionResult SaveRating(int BookRating, int id)
-        {
-            var tblBook =  _context.TblBooks.Find(id);
-            if (tblBook == null)
-            {
-                return NotFound();
-            }
+			_context.TblBorrows.AddAsync(borrow);
+			_context.SaveChangesAsync();
 
-         // double total = tblBook.NumberOfTimesBorrowed * tblBook.AverageRating;
-           // total += total + BookRating;
+			return RedirectToAction("Index", "TblBooks");
 
-            tblBook.NumberOfTimesBorrowed += 1;
-        //   tblBook.AverageRating = (float)(total / tblBook.NumberOfTimesBorrowed);
-            _context.Update(tblBook);
-            _context.SaveChangesAsync();
-
-            return RedirectToAction("Index", "TblBooks");
-
-
-        }
+		}
 
 
 
-    }
+		public ActionResult SaveRating(double BookRating, int id)
+		{
+			var tblBook = _context.TblBooks.Find(id);
+			if (tblBook == null)
+			{
+				return NotFound();
+			}
+
+			if (tblBook.AverageRating == null) 
+			{
+				tblBook.AverageRating = 0;
+			}
+
+			tblBook.NumberOfTimesRated += 1;
+			tblBook.RatingTotal += (float)BookRating;
+			tblBook.AverageRating = (float)(tblBook.RatingTotal/tblBook.NumberOfTimesRated);
+
+			//double total = (float)(tblBook.NumberOfTimesBorrowed * tblBook.AverageRating);
+			//total += total + BookRating;
+
+			//tblBook.NumberOfTimesBorrowed += 1;
+			//tblBook.AverageRating = (float)(total / tblBook.NumberOfTimesBorrowed);
+			_context.Update(tblBook);
+			_context.SaveChangesAsync();
+
+			return RedirectToAction("Index", "TblBooks");
+
+
+		}
+
+
+
+	}
 }
